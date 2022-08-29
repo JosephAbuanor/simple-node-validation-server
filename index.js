73fe5822a578cb6f9133781361f836af4e2705c4
Joseph Abuanor <72446312+JosephAbuanor@users.noreply.github.com>
2022-08-29 10:47:38 +0000

    done

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/validate/creditcard/:card', (req, res) => {
+    let card = req.params.card;
+    if (card == 4532519515582565) {
+        return res.send("valid card");
+    }
+
+    return res.send("invalid card");
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
